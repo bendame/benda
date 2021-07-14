09d282851c59599cdddae3eca8bebc1f94cf5d41
Nathan Tuala <ntuala1@stu.parkland.edu>
2021-07-13 23:10:44 -0500

    Created the modules Chat, Home, and Promo

---
+++ lib/chat.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class Chat extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center (
+      child: Text("It's cloudy here"),
+    );
+  }
+}
---
+++ lib/promo.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Promo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center (
+      child: Text("It's cloudy here"),
+    );
+  }
+}
